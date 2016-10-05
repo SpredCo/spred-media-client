aed666f2979718b60f8b0285ad4d1eea96b0cd90
Guillaume Galas <guillaume.galas@epitech.eu>
2016-10-05 23:04:16 +0200

    charpy room export

--- src/charpy-room.js
+++ src/charpy-room.js
@@ -1,7 +1,11 @@
 var io = require('socket.io-client');
 
-function Room(namespace, options) {
+function create(namespace, options) {
 	options = options || {};
 
 	var wss = io('wss://' + location.host || options.host);
+}
+
+module.export = {
+	create: create
 }
