e5aba3955fea6bf0758ea31d6ae462ee3dd4cc3e
Guillaume Galas <guillaume.galas@epitech.eu>
2016-11-11 18:06:29 +0100

    charpy room log added

--- src/charpy-room.js
+++ src/charpy-room.js
@@ -56,6 +56,7 @@ Room.prototype.present = function() {
 				this.leave();
 			} else {
 				this.webRtcPeer.processAnswer(presenter_answer.sdpAnswer);
+				console.log("Presenter request received and processed : ", presenter_answer.sdpAnswer);
 			}
 		}.bind(this));
 
@@ -68,6 +69,7 @@ Room.prototype.present = function() {
 				wss.emit('presenter_request', {
 					sdpOffer: offerSdp
 				});
+				console.log("An sdpOffer has been sent : ", sdpOffer);
 			});
 		});
 	}
