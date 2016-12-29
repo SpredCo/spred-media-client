8edfefae90e8cc312826173b737dc93f12b4dc78
Guillaume Galas <guillaume.galas@epitech.eu>
2016-12-29 16:14:56 +0100

    adding media.spred.tv host

--- src/models/spred-client.js
+++ src/models/spred-client.js
@@ -99,7 +99,7 @@ SpredClient.prototype.sendNotification = function(object) {
 }
 
 function etablishMediaServiceConnection(token) {
-	this.wss = io("http://localhost:3030/");
+	this.wss = io("https://media.spred.tv/");
 
 	this.wss.on('connect_error', function(err) {
 		console.error(`Got an error: ${err}`);
